029c209926de7913ce480083cff41378cbd069f4
Jared Beckler <jared.beckler@gmail.com>
2016-03-18 09:31:11 -0700

    add submit event to gather user inputted GitHub username

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,11 @@
 var apiKey = require('./../.env').apiKey;
-var getRepos = require('./../get-repos-interface.js').getRepos;
+// var getRepos = require('./../get-repos-interface.js').getRepos;
 
 $(document).ready(function(event){
-
+  $('#githubUsername').submit(function(event){
+    event.preventDefault();
+    var username = $('#username').val();
+    $('#username').val("");
+    console.log(username);
+  });
 });
