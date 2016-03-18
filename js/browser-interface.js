70efc89357a178c4dde46d8830173275c4111b59
Jared Beckler <jared.beckler@gmail.com>
2016-03-18 09:21:48 -0700

    add form and display areas to html page

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,3 +1,6 @@
-$(document).ready (function(event){
+var apiKey = require('./../.env').apiKey;
+var getRepos = require('./../get-repos-interface.js').getRepos;
+
+$(document).ready(function(event){
 
 });
